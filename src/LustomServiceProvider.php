8e9a3ac92dd547a063d219be70f536dfd0fb3477
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2019-06-24 13:35:20 +0630

    Add config and publish it

--- src/LustomServiceProvider.php
+++ src/LustomServiceProvider.php
@@ -11,9 +11,8 @@ class LustomServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register() {
+        $this->mergeConfigFrom(__DIR__ . '/config.php', 'lustom');
     }
 
     /**
@@ -21,8 +20,9 @@ class LustomServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot() {
+        $this->publishes([
+            __DIR__ . '/config.php' => config_path('lustom.php')
+        ]);
     }
 }
---
+++ src/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'root' => '_core',
+    'namespace' => 'Core'
+];
